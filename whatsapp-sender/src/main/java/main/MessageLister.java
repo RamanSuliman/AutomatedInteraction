6d41ee29b3701ef1f291d4c71c75826e15d50f24
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-01-25 23:53:42 +0000

    Initiate Commit.

---
+++ whatsapp-sender/src/main/java/main/MessageLister.java
@@ -0,0 +1,9 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MessageLister 
+{
+	
+}
---
+++ whatsapp-sender/src/main/java/main/Reader.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class Reader 
+{
+
+  
+}
