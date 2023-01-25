62d7e7689422e88b47518df02f9659c90103e581
raman suliman <75031065+RamanSuliman@users.noreply.github.com>
2023-01-25 23:56:12 +0000

    Setup the keylogger functionality to listen for keyboard presses.

--- whatsapp-sender/src/main/java/main/Reader.java
+++ whatsapp-sender/src/main/java/main/Reader.java
@@ -13,6 +13,35 @@ import org.jnativehook.keyboard.NativeKeyListener;
 
 public class Reader 
 {
+	  public static void main(String[] args) throws Exception 
+	  {
+		  System.out.println(MessageLister.messages.size());
+		  //Start the program 
+		  new Reader();
+	  }
+	  
+	  public Reader()
+	  {
+		  keyLogger();
+	  }
+	  
+	  private void keyLogger()
+	  {
+		  try {
+			  GlobalScreen.registerNativeHook();
+		  }catch (Exception e) {
+			e.printStackTrace();
+		  }
+		  GlobalScreen.addNativeKeyListener(new NativeKeyListener() {
+			
+			public void nativeKeyTyped(NativeKeyEvent e) {}
+			public void nativeKeyPressed(NativeKeyEvent e	) {}
+			
+			public void nativeKeyReleased(NativeKeyEvent e) 
+			{
+					
+			}
+		}); 
+	  }
 
-  
 }
